eb5adf068eead8ac0f84a40835217eb32c2aff6d
sander <sander@BEST-Laptop>
2020-01-22 15:29:58 +0100

    logging

--- src/main/java/com/cap/subclasses/MainClass.java
+++ src/main/java/com/cap/subclasses/MainClass.java
@@ -1,21 +1,29 @@
 package com.cap.subclasses;
 
+import org.apache.log4j.Logger;
+
 public class MainClass {
+	private static final Logger LOGGER = Logger.getLogger(MainClass.class);
+	
     SubClass subClass;
 
     protected MainClass(SubClass subClass) {
+    	LOGGER.info("in MainClass");
         this.subClass = subClass;
     }
 
     public MainClass(){
+    	
         this(new SubClass());
     }
 
     public int doWork() {
+    	LOGGER.debug("in doWork");
         return privateDoWork();
     }
 
     private int privateDoWork() {
+    	LOGGER.warn("in privateDoWork");
         return subClass.actualWork();
     }
 }
